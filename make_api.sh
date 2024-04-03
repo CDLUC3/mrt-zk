3a5337f46309b20f881315c41a8cea1c842774bf
Terry Brady <terrence.brady@ucop.edu>
2024-04-02 17:07:52 -0700

    resources

--- make_api.sh
+++ make_api.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-rm -rf api
-mkdir -p api/java
 mvn clean javadoc:javadoc
-cp -r target/site/apidocs/* api/java
-mkdir -p api/ruby
 cd src/main/ruby
 bundle exec rdoc merritt_zk.rb
 cd ../../..
+rm -rf api
+mkdir -p api/java api/ruby
+cp -r target/site/apidocs/* api/java
 cp -r src/main/ruby/doc/* api/ruby
