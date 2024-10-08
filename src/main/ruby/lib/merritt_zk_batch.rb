83e808d52cbb9cfe1b477bd81b9b0a700bf9614a
Terry Brady <terrence.brady@ucop.edu>
2024-10-08 13:31:13 -0700

    batch job count

--- src/main/ruby/lib/merritt_zk_batch.rb
+++ src/main/ruby/lib/merritt_zk_batch.rb
@@ -202,6 +202,12 @@ module MerrittZK
           batchjson = batch.data
           batchjson[:id] = batch.id
           batchjson[:status] = batch.status_name
+          batchjson[:jobCountFailed] = batch.get_failed_jobs(zk).length
+          batchjson[:jobCountDeleted] = batch.get_deleted_jobs(zk).length
+          batchjson[:jobCountCompleted] = batch.get_completed_jobs(zk).length
+          batchjson[:jobCountProcessing] = batch.get_processing_jobs(zk).length
+          batchjson[:jobCount] = batch.get_processing_jobs(zk).length +
+            batch.get_failed_jobs(zk).length + batch.get_deleted_jobs(zk).length + batch.get_completed_jobs(zk).length
           batches.append(batchjson)
         rescue StandardError => e
           puts "List Batch #{cp} exception: #{e}"
